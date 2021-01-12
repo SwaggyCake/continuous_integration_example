e92dcf3ba0102d55a653abc83ad6b53701f07fd5
SwaggyCake <quent73230@gmail.com>
2021-01-12 17:45:39 +0100

    exo1

--- sum_test.py
+++ sum_test.py
@@ -7,6 +7,8 @@ def test_sum_floats():
     assert mysum([0.1, 1.2, 2.3, 3.4, 4.5]) == 11.5
 def test_sum_nothing():
     assert mysum([]) == 0
+def test_type_sum_integers():
+    assert type(mysum([0, 1, 2, 3, 4])) == int
 
 
 import pytest
