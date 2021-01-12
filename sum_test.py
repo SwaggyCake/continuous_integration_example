c7e0cda0f73ef3194981efbafc6c83cfd57f6aca
SwaggyCake <quent73230@gmail.com>
2021-01-12 20:49:25 +0100

    +ts=

--- sum_test.py
+++ sum_test.py
@@ -1,16 +1,14 @@
 from sum import mysum
+import pytest
 
 
-def test_sum_integers():
-    assert mysum([0, 1, 2, 3, 4]) == 10
-def test_sum_floats():
-    assert mysum([0.1, 1.2, 2.3, 3.4, 4.5]) == 11.5
-def test_sum_nothing():
-    assert mysum([]) == 0
-def test_type_sum_integers():
-    assert type(mysum([0, 1, 2, 3, 4])) == int
-
-def test_type_sum():
-    assert mysum([0, 1, 2, 3, 4]) > 0
-
-
+@pytest.mark.parametrize('numbers, output', [
+    ([1, 2.1, 3], 6),
+    ([1, 2, 3, 4, 5], 15)
+    # Ajoutez des tests pour que la somme des éléments de la liste soit égale à 10, 24 et 27
+])
+def test_mysum(numbers, output):
+    assert mysum(numbers) == output
+    for num in numbers:
+        assert type(num) == int
+    assert (len(numbers) % 2) == 1
